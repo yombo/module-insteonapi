4b3cd4d8dd41ad0dfae788d8195521321db30ae2
Mitch Schwenk <mitch@yombo.net>
2016-01-04 23:51:33 -0800

    Work in progress. Doesn't work just yet. Checkpoint.

--- insteonapi.py
+++ insteonapi.py
@@ -1,5 +1,5 @@
 #This file was created by Yombo for use with Yombo Gateway automation
-#software.  Details can be found at http://www.yombo.net
+#software.  Details can be found at http://yombo.net
 """
 Insteon API
 ===========
@@ -23,7 +23,7 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-The **`Yombo.net <http://www.yombo.net/>`_** team and other contributors
+The `Yombo.net <http://www.yombo.net/>`_ team and other contributors
 hopes that it will be useful, but WITHOUT ANY WARRANTY; without even the
 implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
@@ -37,7 +37,7 @@ Implements
 - class InsteonAPI - the command module 
 
 .. moduleauthor:: Mitch Schwenk <mitch-gw@yombo.net>
-:copyright: Copyright 2012 by Yombo.
+:copyright: Copyright 2012-2013 by Yombo.
 :license: GPL(v3)
 """
 import time
@@ -46,53 +46,66 @@ import time
 from yombo.core.module import YomboModule
 from yombo.core.sqldict import SQLDict
 from yombo.core.helpers import getComponent, getInterfaceModule
-from yombo.core.db import get_dbtools
 from yombo.core.log import getLogger
 
-logger = getLogger()
+logger = getLogger("modules.insteonapi")
 
 class InsteonCmd:
     """
     An Insteon command instance that is passed between this module
     and any interface modules to send insteon commands to the power line.
+    
+    :ivar insteonaddress: Insteon unit address.
+    :type insteonaddress: string
     """
-    def __init__(self, insteonapi, message = {'msgID':None}):
+    def __init__(self, apimodule, message = {'msgID':None}):
         """
         Setup the class to communicate between insteon API module and any interface modules.
         
-        @param insteonapi: A pointer to the insteonapi class.
+        :param insteonapi: A pointer to the insteonapi class.
+        :type insteonapi: instance
+        :param message: ???
+        :type message: ??
         """
+        self.deviceobj = message.payload['deviceobj']
+        """@ivar: The device itself
+           @type: C{YomboDevice}"""
+        self.cmdobj = message.payload['cmdobj']
+        """@ivar: The command itself
+           @type: C{YomboDevice}"""
+        self.deviceClass = "insteon"
 
-        self.insteonaddress = None
+        self.address = self.deviceobj.deviceVariables['insteonaddress']['value'][0].upper()
         """@ivar: Insteon unit address.
            @type: C{string}"""
-        self.insteoncommand = None
-        """@ivar: Information regarding the command. "origcmd" is the original command. "value" is the offical
-             insteon command.
-           @type: C{dict}"""
-        self.msguuid = message.msgID
+        self.command = message['payload']['cmdobj'].cmd.upper()
+        """@ivar: Text representing the command - on, off, dim, etc.
+           @type: C{string}"""
+
+        self.msguuid = message.msgUUID
         """@ivar: The msgID that was generated to create this command. Is None if comming from interface module.
            @type: C{str}"""
-        self.insteonextended = None
+        self.extended = None
         """@ivar: Extended data to send with the insteon command.
            @type: C{hex}"""
-        self.insteonvalue = None
+        self.deviceState = None
         """@ivar: The updated state of the device. This is set by the interface module.
            @type: C{str} or C{int}"""
-        self.deviceid =  message['payload']['deviceid']
-        self.devicetype = None
-        """@ivar: Either 'x10' or 'insteon'
-           @type: C{str}"""
-        self.chain = {}  # AKA history
-        self.created = time.time()
+        self.interfaceResult = None
+        """@ivar: Interface Result. This is set by the interface module.
+           @type: C{str} or C{int}"""
+        self.commandResult = None
+        """@ivar: The result from the command. Will be set by the interface module.
+           @type: C{str} or C{int}"""
+
+        self.created = int(time.time())
         self.interfaceResult = None
         self.commandResult = None
-        self.insteonapi = insteonapi
-        self.originalMessage = message
-        self.deviceid = None
         self.status1 = None
         self.status2 = None
-        
+        self.originalMessage = message
+        self.apimodule = apimodule
+
     def dump(self):
         """
         Convert key class contents to a dictionary.
@@ -100,18 +113,23 @@ class InsteonCmd:
         @return: A dictionary of the key class contents.
         @rtype: C{dict}
         """
-        return {'insteonaddress': self.insteonaddress,
-                'insteoncommand': self.insteoncommand,
-                'chain': self.chain,
-                'created': self.created,
+        return {'address': self.address,
+                'command': self.command,
+                'extended': self.extended,
+                'deviceState': self.deviceState,
                 'interfaceResult': self.interfaceResult,
-                'commandResult': self.commandResult }
+                'commandResult': self.commandResult,
+                'deviceobj': self.deviceobj,
+                'cmdobj': self.cmdobj,
+                'deviceClass': self.deviceClass,
+                'created': self.created,
+                }
                 
     def sendCmdToInterface(self):
         """
         Send to the insteon command module
         """
-        pass
+        self.apimodule.interfaceModule.sendInsteonCmd(self)
         
     def statusReceived(self, status, statusExtended={}):
         """
@@ -119,23 +137,22 @@ class InsteonCmd:
         """
         pass
         
-    def cmdDone(self):
+    def done(self):
         """
-        Every command should tell the sender when the command was sent.
+        Called by the interface module once the command has been completed.
         
-        We just need to validate that it was sent, some protocols are
-        only one way.  This way the user gets some feedback.
-        
-        In this case, insteon may not work, but at the command was issued.
+        Note: the interface module will call the insteonapi module when a device
+        has a status change.  This is a different process then a command.
         """
-        self.insteonapi.cmdDone(self)
-        self.insteonapi.removeInsteonCmd(self)
+        self.apimodule.cmdDone(self)
+        self.apimodule.removeInsteonCmd(self)
 
     def cmdPending(self):
         """
         Used to tell the sending module that the command is pending (processing)
         """
-        pass
+        reply = self.originalMessage.getReply(msgStatus='processing', msgStatusExtra="interface module processing request")
+        reply.send()
     
     def cmdFailed(self, statusmsg):
         """
@@ -143,99 +160,136 @@ class InsteonCmd:
         
         statussg should hold the failure reason.  Displayed to user.
         """
-        pass
-
+        reply = self.originalMessage.getReply(msgStatus='failed', msgStatusExtra="interface module failed to process request")
+        self.apimodule.removeInsteonCmd(self)
+        reply.send()
+        
 class InsteonAPI(YomboModule):
     """
     Insteon Command Module
     
     Generic module that handles all insteon commands from other modules
-    and prepares it for an interface module.  Also recieves data from
+    and prepares it for an interface module.  Also receives data from
     interface modules for delivery to other gateway modules.
     """
 
-    def init(self):
+    def _init_(self):
+#        logger.info("&&&&: Insteon Module Devices: {devices}", devices=self._Devices)
+#        logger.info("&&&&: Insteon Module DeviceTypes: {devicesTypes}", deviceTypes=self._DeviceTypes)
         self._ModDescription = "Insteon API command interface"
         self._ModAuthor = "Mitch Schwenk @ Yombo"
         self._ModUrl = "http://www.yombo.net"
 
-        self._RegisterDistributions = ['cmd']
+#        self._RegisterDistributions = ['cmd']
         self.insteoncmds = {}         #store a copy of active x10cmds
-        self.dbtools = get_dbtools()
         self.originalMessage = None
+        self.interfaceSupport = False
+
         self.deviceLinks = SQLDict(self,"deviceLinks")  # used to store what devices
                                           # listen to group broadcasts.
                                           # data saved in this dict is saved
                                           # in SQL and recreated on startup.
 
-        self.functionToInsteon = {
-          'ON'            : 0x02,
-          'OFF'           : 0x03,
-          'DIM'           : 0x04,
-          'BRIGHTEN'      : 0x05,
-          'MICRO_DIM'     : None,
-          'MICRO_BRIGHTEN': None }
+        self.insteonDeviceTypes = {
+          'Insteon Appliance' : "Ps4joEmGM129qUo7Ra2YigmT",
+          'Insteon Lamp' : "DTlJJrGOBp8SxBCQYSepfcA4",
+          }
 
-        self._housecodes = list('ABCDEFGHIJKLMNOP')
-        self._unitcodes = range(1, 17)
-        
-#@todo: create irigation, alarm, etc types.
-        self.deviceTypes = {
-          1 : "insteonApplicance",
-          2 : "insteonLamp"}        
+        self.insteonDevices = {} # used to lookup insteon address to a device
+#        for devkey, device in self._LocalDevicesByUUID.iteritems():
+#            self.insteonDevices[device.deviceVariables['address'][0].upper()] = device
 
-        self.x10DeviceTypes = [1,2,18]
-        self.insteonDeviceTypes = [16,17]
-        
-#@todo: get devicetypecommand mappings from database
-            
-    def load(self):
-#        logger.debug("@#@#@#@#@#@#@#@:  %s", getInterfaceModule(self))
-#        self.interfaceModule = getComponent(getInterfaceModule(self))
+    def _load_(self):
+        try:
+            interfaceModule = self._DeviceTypes.itervalues().next()  # Just get any deviceType as they all are routed to the same interface module
+            self.interfaceModule = self._ModulesLibrary.getDeviceRouting(interfaceModule[0]['devicetypeuuid'], 'Interface', 'module')
+            self.interfaceSupport = True
+            self._reload_()
+        except:
+            # no X10API module!
+            logger.warn("Insteon API - No Insteon interface module found, disabling Insteon support.")
+            self.interfaceSupport = False
 
-        self.interfaceModule = getComponent("yombo.gateway.modules.InsteonPLM")
-        
-    def start(self):
+    def _reload_(self):
+        if self.interfaceSupport:
+            self.insteonDevices.clear()
+            for devkey, device in self._Devices.iteritems():
+                logger.info("devicevariables: {vars}", vars=device.deviceVariables)
+                iaddress = device.deviceVariables['insteonaddress']['value'][0].upper()
+                self.x10devices[address] = device
+
+    def _start_(self):
         logger.debug("Insteon API command module started") 
         
-    def stop(self):
+    def _stop_(self):
         pass
 
-    def unload(self):
+    def _unload_(self):
         pass
 
     def message(self, message):
-        logger.debug("InsteonAPI got message: %s", message.dump())
+        logger.debug("InsteonAPI got message: {message}", message=message.dump())
         if message.msgType == 'cmd' and message.msgStatus == 'new':
-            deviceid = message['payload']['deviceid']
-            if self._Devices[deviceid].devicetypeid in self.x10DeviceTypes:
-                self.processNewCmdMsg(message, 'x10')
-            elif self._Devices[deviceid].devicetypeid in self.insteonDeviceTypes:
-                self.processNewCmdMsg(message, 'insteon')
+            if message.payload['deviceobj'].deviceUUID in self._Devices:
+#              try:
+                self.processNewCmdMsg(message)
+#              except:
+#                logger.info("")
 
-    def processNewCmdMsg(self, message, type):
-        logger.debug("msg: %s", message.dump())
+    def processNewCmdMsg(self, message):
+        logger.debug("in insteponapi::processNewCmdMsg")
 
         insteonCmd = InsteonCmd(self, message)
-        insteonCmd.insteonaddress = self._Devices[message['payload']['deviceid']].deviceaddress
-        insteonCmd.insteoncommand = message['payload']['cmd'].lower()
-        insteonCmd.devicetype = type
 
-        self.insteoncmds[message.msgID] = insteonCmd
-#        logger.debug("NEW: x10cmd: %s", x10cmd.dump())
 
-        self.interfaceModule.sendInsteonCmd(message.msgID)
+        self.insteoncmds[message.msgUUID] = insteonCmd
+        logger.info("NEW: insteonCmd: {insteonCmd}", insteonCmd=insteonCmd.dump())
 
-    def statusReceived(self, x10cmd):
+#        x10cmd.deviceobj.getRouting('interface')
+#        self._ModulesLibrary.getDeviceRouting(x10cmd.deviceobj.deviceTypeUUID, 'Interface')
+        self.interfaceModule.sendInsteonCmd(insteonCmd)
+
+    def statusUpdate(self, address, command, status=None):
         """
-        Used to deliver device state changes. A lamp turns on, etc.
-
-        The value of payload will be the final state value of
-        the X10 device.
+        Called by interface modules when a device has a change of status.
         """
-        pass
+        if address in self.insteonDevices:
+          device =  self.insteonDevices[address]
+          newstatus = None
+          tempcmd = command.upper()
 
-    
+          if device.deviceTypeUUID == self.insteonDeviceTypes["Insteon Lamp"]:
+            if tempcmd == 'ON':
+              newstatus = 'ON'
+            elif tempcmd == 'OFF':
+              newstatus = 'OFF'
+          elif device.deviceTypeUUID == self.insteonDeviceTypes["Insteon Appliance"]:
+            if tempcmd == 'ON':
+              newstatus = 100
+            elif tempcmd == 'OFF':
+              newstatus = 0
+            elif tempcmd == 'DIM':
+              if type(device.status[0]['status']) is int:
+                  newstatus = device.status[0]['status'] - 12
+              else:
+                  newstatus = 88
+            elif tempcmd == 'BRIGHT':
+              if type(device.status[0]['status']) is int:
+                  newstatus = device.status[0]['status'] + 12
+              else:
+                  newstatus = 100
+
+            if type(newstatus) is int:
+              if newstatus > 100:
+                  newstatus = 100
+              elif newstatus < 0:
+                  newstatus = 0
+            else:
+                newstatus = 0
+
+          logger.debug("status update... {newstatus}", newstatus=newstatus)
+          device.setStatus(status=newstatus, source="x10api")
+
     def cmdDone(self, insteonCmd):
         """
         Called after interface module reports the command was sent out.
@@ -243,39 +297,9 @@ class InsteonAPI(YomboModule):
         First update the device status value, then sent a cmdreply msg.
         Finally, send a status message.
         """
-        tempcmd = insteonCmd.insteoncommand
-        deviceid = insteonCmd.deviceid
-        newstatus = None
-#@todo: Move to interface module!!!
-                
-        self._Devices[deviceid].setStatus(silent=True, status=insteonCmd.status1)
-
-        # 1 - reply to sender so they know we are done.
-        replmsg = insteonCmd.originalMessage.getReply()
-        replmsg.msgStatusExtra = 'done'
-        replmsg.payload = {'status'  : self._Devices[deviceid].status,
-                           'cmd'     : insteonCmd.insteoncommand['origcmd'],
-                           'deviceid': deviceid }
-        logger.debug("insteon cmddone msgreply: %s", replmsg.dump())
+        replmsg = insteonCmd.originalMessage.getReply(status='done', statusExtra="Command completed.")
+        logger.debug("msgreply: {msgreply}", msgreply=replmsg.dump())
         replmsg.send()
-
-        # 2 - let the rest of the world know.
-        self._Devices[deviceid].sendStatus(src=self.fname)
-
-    def cmdPending(self, x10cmd, statusmsg):
-        """
-        Used to tell the sending module that the command is pending.
-        
-        Used when it's taking longer than 1 second.  This lets the other
-        module/client/device know we received the command, but it's still
-        processing.
-        
-        A cmdDone or cmdFail is expected afterward.
-        
-        Statusmsg should contain the actual status to display to a user
-        or to report in a log.
-        """
-        pass
 
     def cmdFailed(self, x10cmd, statusmsg="Unknown reason."):
         """
@@ -286,10 +310,18 @@ class InsteonAPI(YomboModule):
         """
         pass
     
+    def removeX10Cmd(self, x10cmd):
+        """
+        Delete an old x10cmd object
+        """
+        logger.debug("Purging x10cmd object: {x10cmd}", x10cmd=x10cmd.x10uuid)
+        del self.x10cmds[x10cmd.x10uuid]
+        logger.debug("pending x10cmds: {x10cmds}", x10cmds=self.x10cmds)
+    
     def removeInsteonCmd(self, insteonCmd):
         """
         Delete an old x10cmd object
         """
-        logger.debug("Purging x10cmd object: %s", insteonCmd.msguuid)
+        logger.debug("Purging insteon command instance: {msguuid}", msguuid=insteonCmd.msguuid)
         del self.insteoncmds[insteonCmd.msguuid]
-        logger.debug("pending insteonCmds: %s", self.insteoncmds)
+        logger.debug("pending insteonCmds: {insteoncmds}", insteoncmds=self.insteoncmds)
